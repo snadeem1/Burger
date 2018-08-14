27f328951da86b42a6be6dedc14826535c531a7d
Sitwat <sitwat.nadeem1@gmail.com>
2018-08-13 20:45:10 -0400

    models

--- config/orm.js
+++ config/orm.js
@@ -11,7 +11,27 @@ var orm = {
         });
     
       },
-}
+
+    insertOne: function(burger_name, cb){
+        connection.query('INSERT INTO burgers SET ?', {
+            burger_name: burger_name,
+            devoured: false,
+          }, function (err, result) {
+            if (err) throw err;
+            cb(result);
+          });
+      
+        },
+
+    updateOne: function(burgerID, cb){
+        connection.query('UPDATE burgers SET ? WHERE ?', [{devoured: true}, {id: burgerID}], function (err, result) {
+                if (err) throw err;
+                callback(result);
+              });
+        
+          }
+        
+};
 
 
 
